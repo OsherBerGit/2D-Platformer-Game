c995828881b71d96679c12b629edeaed29ccccf2
OsherBerGit <bergerosher10@gmail.com>
2025-10-30 23:35:26 +0200

    Initial commit

---
+++ scenes/game objects/enemy.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+@onready var game_manager = %GameManager
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		var y_delta = position.y - body.position.y
+		var x_delta = body.position.x - position.x
+		if y_delta > 30:
+			print("Destroy Enemy")
+			queue_free()
+			body.jump()
+		else:
+			print("Decrese player health")
+			game_manager.decrease_health()
+			if x_delta > 0:
+				body.jump_side(500)
+			else:
+				body.jump_side(-500)
---
+++ scenes/game objects/main_character.gd
@@ -0,0 +1,63 @@
+extends CharacterBody2D
+
+const SPEED = 400.0 # 300.0
+const JUMP_VELOCITY = -900.0 # -400.0
+@onready var sprite_2d: AnimatedSprite2D = %Sprite2D
+@export var particle : PackedScene
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jump_count = 0
+
+func jump():
+	velocity.y = JUMP_VELOCITY
+	spawn_particle()
+	
+func jump_side(x):
+		velocity.y = JUMP_VELOCITY
+		velocity.x = x
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if is_on_floor():
+		jump_count = 0
+		
+		# Animations
+		if (velocity.x > 1 || velocity.x < -1):
+			sprite_2d.animation = "running"
+		else:
+			sprite_2d.animation = "default"
+		
+	else:
+		velocity += get_gravity() * delta
+		if jump_count == 2:
+			sprite_2d.animation = "double_jumping"
+		else:
+			sprite_2d.animation = "jumping"
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and jump_count < 2:
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+		if jump_count == 2:
+			spawn_particle()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 12) # SPEED
+
+	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
+	
+func spawn_particle():
+	var particle_node = particle.instantiate()
+	particle_node.position = position
+	get_parent().add_child(particle_node)
+	await get_tree().create_timer(0.3).timeout
+	particle_node.queue_free()
